d4f63e73cf64c2e3d94014a6e19b26b145eb5ce9
Riccardo Cinaglia <cinagliariccardo@gmail.com>
2025-02-21 00:36:09 +0100

    fix files

--- src/cockpit/modules/Assets/cli.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-if (!isset($cli, $app) || PHP_SAPI !== 'cli') {
-    return;
-}
-
-$cli->add(new Assets\Command\FixVisibilityPermissions($app));
-$cli->add(new Assets\Command\Thumbhash($app));
--- src/cockpit/modules/Updater/cli.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-if (!isset($cli, $app) || PHP_SAPI !== 'cli') {
-    return;
-}
-
-$cli->add(new Updater\Command\App\Update($app));
